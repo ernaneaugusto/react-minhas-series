fd1d03df8d1744b587c29c27209f0ff31dc02007
ernaneaugusto <ernaneatcipriano@gmail.com>
2020-07-26 22:04:51 -0300

    criacao do projeto, config mdbootstrap e cricao menu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'mdbootstrap/css/bootstrap.min.css';
+import 'mdbootstrap/css/mdb.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar navbar-expand navbar-dark primary-color">
+        <a className="navbar-brand" href="#">Minhas Séries</a>
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Séries</a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
